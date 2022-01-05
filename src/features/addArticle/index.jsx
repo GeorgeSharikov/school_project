d27fa638fb8a911918e63dee8a2c91b23d834781
George <g-sharikov@bk.ru>
2022-01-05 19:04:14 +0300

    Made Add Article button

--- src/features/addArticle/index.jsx
+++ src/features/addArticle/index.jsx
@@ -1,9 +1,19 @@
-import style from './ui.module.css'
+import style from './ui/ui.module.css'
+import {Button, LinkStyled} from "./ui/StyledComponents.js";
+import {Link} from "react-router-dom";
+import Typography from "@mui/material/Typography";
 
 export const AddArticleButton = () => {
     return (
-        <div>
-            <div></div>
-        </div>
+        <Button>
+            <LinkStyled to={'/'}>
+                <Typography
+                    sx={{p: '0 16px'}}
+                    component={'div'}
+                >
+                    Новая запись
+                </Typography>
+            </LinkStyled>
+        </Button>
     )
 }
---
+++ src/features/addArticle/ui/StyledComponents.js
@@ -0,0 +1,23 @@
+import styled from "@mui/material/styles/styled.js";
+import {Link} from "react-router-dom";
+
+export const Button = styled('div')(() => ({
+    outline: 0,
+    textAlign: 'center',
+    display: 'inline-flex',
+    backgroundColor: '#fff',
+    color: '#000',
+    fontSize: '16px',
+    height: '40px',
+    borderRadius: '8px',
+    userSelect: 'none',
+    boxShadow: '0 1px 1px rgb(0 0 0 / 10%), 0 2px 5px rgb(0 0 0 / 5%), 0 -1px 0 rgb(0 0 0 / 5%), -1px 0 0 rgb(0 0 0 / 10%), 1px 0 0 rgb(0 0 0 / 10%)',
+}))
+
+export const LinkStyled = styled(Link)(() => ({
+    fontWeight: 700,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    cursor: 'pointer',
+}))
