d5264de4e96d9d7ee31d3f7d12c79ca35ee601f1
George <g-sharikov@bk.ru>
2022-01-16 19:35:09 +0300

    Added user icon in the header

---
+++ src/features/navigationUser/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+
+export const NavigationUser = (props) => {
+    return (
+        <div>
+            <AccountBoxIcon sx={{fontSize: '40px', color: 'black', cursor: 'pointer'}}/>
+        </div>
+    );
+}
