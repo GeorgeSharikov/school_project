9f0766919aee21563ab39e8b6208af09c54d69bc
George <g-sharikov@bk.ru>
2022-01-19 19:52:11 +0300

    added sideBarToggle selector

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import {sideBarToggleSlice} from "../features/sidebarToggle/model/slice.js";
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        sideBarToggle: sideBarToggleSlice
+    }
 })
