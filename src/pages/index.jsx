f8c09d8804acfceba81d3600a2764a9fb4821b0e
George <g-sharikov@bk.ru>
2022-01-30 02:45:46 +0300

    Added article's feed path to routes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,9 @@
-import {Routes} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import {SideBar} from "../widgets/Sidebar";
 import styles from './main.module.css'
 import {useSelector} from "react-redux";
 import {sideBarSelectors} from "../features/sidebarToggle/model/slice.js";
+import {ArticlesFeed} from "./ArticlesFeed";
 
 export const Routing = () => {
     const isSidebarActive = useSelector(sideBarSelectors.getSidebarIsActive)
@@ -12,7 +13,7 @@ export const Routing = () => {
             <div className={styles.content}>
                 <div className={styles.wrapper}>
                     <Routes>
-
+                        <Route path={'/articles'} element={<ArticlesFeed />}/>
                     </Routes>
                 </div>
             </div>
