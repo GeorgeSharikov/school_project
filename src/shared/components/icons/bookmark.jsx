3b5728201b87f13d661b49c9aa99732217dd0fdf
George <g-sharikov@bk.ru>
2022-01-30 20:26:25 +0300

    delete shared icons

--- src/shared/components/icons/bookmark.jsx
+++
@@ -1,6 +0,0 @@
-import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
-import { iconsStyle } from '../../../constants/styles';
-
-export const Bookmark = () => {
-    return <BookmarkBorderIcon style={iconsStyle}/>
-}
--- src/shared/components/icons/commentsIcon.jsx
+++
@@ -1,6 +0,0 @@
-import CommentOutlinedIcon from '@material-ui/icons/Comment';
-import { iconsStyle } from '../../../constants/styles';
-
-export const ComsIcon = () => {
-    return <CommentOutlinedIcon style={iconsStyle}/>
-}
