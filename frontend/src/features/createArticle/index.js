77bd1cbd3d8d5ee703b543b9c76f4e68709e65e5
George <g-sharikov@bk.ru>
2022-02-27 12:36:20 +0300

    Added article's saving

--- frontend/src/features/createArticle/index.js
+++ frontend/src/features/createArticle/index.js
@@ -8,15 +8,27 @@ export const ArticleEditor = (props) => {
     const editor = new EditorJS(settings)
     const inputRef = useRef()
 
+    const save = async () => {
+        try{
+            const data = await editor.save()
+            const title = document.getElementById('titleArea').value
+            console.log({data, title})
+        }catch (e) {
+            console.log('Saving failed: ', e)
+        }
+    }
+
     useEffect(() => {
         inputRef.current.focus()
+        document.getElementById("editorjs").childNodes[0].remove()
     }, [])
+
     return (
         <div className={styles.editorContainer}>
             <div className={`${styles.Ieditor} ${styles.editorAuthors}`}>
                 <div className={styles.authorSelect}>
                     <span className={styles.selectImg}>
-                        <img src={'https://leonardo.osnova.io/aa291fee-0c9a-4803-c179-af2bda5cb297/'}/>
+                        <img src={'https://leonardo.osnova.io/aa291fee-0c9a-4803-c179-af2bda5cb297/'} alt={'sub'}/>
                     </span>
                     <span className={styles.authorName}>
                         Мой блог
@@ -28,11 +40,24 @@ export const ArticleEditor = (props) => {
             </div>
             <div className={styles.editorContent}>
                 <div className={styles.Ieditor}>
-                    <textarea rows="1" ref={inputRef} placeholder="Заголовок" maxLength="120" className={styles.inputTitle} style={{height: 47, overflowY: 'hidden'}}/>
+                    <textarea rows="1"
+                              id={'titleArea'}
+                              ref={inputRef}
+                              placeholder="Заголовок"
+                              maxLength="120"
+                              className={styles.inputTitle}
+                              style={{height: 47, overflowY: 'hidden'}}
+                              key={1}/>
                 </div>
                 <div id={'editorjs'} className={`${styles.editorJS}`}/>
             </div>
-            <div>a</div>
+            <div className={`${styles.Ieditor} ${styles.editorBottom}`}>
+                <div className={styles.bottomContent}>
+                    <button onClick={save} className={styles.publishButton}>
+                        <span className={styles.buttonLabel}>Опубликовать</span>
+                    </button>
+                </div>
+            </div>
         </div>
 
     );
