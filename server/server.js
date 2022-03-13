9e7b6beb231c2977394bc45a8eb76c66a03e3836
George <g-sharikov@bk.ru>
2022-03-13 20:29:00 +0300

    Added middlewares

--- server/server.js
+++ server/server.js
@@ -2,11 +2,18 @@ import express from 'express'
 import env from 'dotenv'
 env.config()
 import {mainRouter} from "./routes/index.js";
+import cors from 'cors'
+import fileupload from 'express-fileupload'
+import path from 'path'
 
 const PORT = process.env.PORT || 4000
 
 const app = express()
 
+app.use(express.json())
+app.use(fileupload({}))
+app.use(express.static(path.resolve(path.dirname(''), 'Static')))
+app.use(cors())
 app.use('/api', mainRouter)
 
 const start = async () => {
