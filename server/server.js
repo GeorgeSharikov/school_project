c225a6276fab4e6f02b22dee991b8fafa6627a18
George <g-sharikov@bk.ru>
2022-03-21 20:32:42 +0300

    Added Error Handler and Connection to db

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ import {mainRouter} from "./routes/index.js";
 import cors from 'cors'
 import fileupload from 'express-fileupload'
 import path from 'path'
+import {ErrorHandler} from "./middleware/ErrorHandlerMiddleware.js";
+import {sequelize} from "./db/dbConnection.js";
 
 const PORT = process.env.PORT || 4000
 
@@ -15,9 +17,13 @@ app.use(fileupload({}))
 app.use(express.static(path.resolve(path.dirname(''), 'Static')))
 app.use(cors())
 app.use('/api', mainRouter)
+app.use(ErrorHandler)
 
 const start = async () => {
     try{
+        await sequelize.authenticate()
+        await sequelize.sync()
+        // await sequelize.close()
         app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
     }catch (e) {
         console.log(e)
