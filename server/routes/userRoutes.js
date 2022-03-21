9281cb0d6d23932e95c9767479c86e899b13e9dc
George <g-sharikov@bk.ru>
2022-03-21 21:24:02 +0300

    Added log in path

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,5 +4,6 @@ import {UserController} from "../controllers/userController.js";
 const router = new Router()
 
 router.post('/registration', UserController.registration)
+router.post('/login', UserController.logIn)
 
 export const userRouter = router
