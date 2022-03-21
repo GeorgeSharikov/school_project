4015273057cab72d1c4d9d4fd4108f1ad99334e0
George <g-sharikov@bk.ru>
2022-03-21 20:32:15 +0300

    Created user and article models

--- server/db/models/models.js
+++ server/db/models/models.js
@@ -1,27 +1,29 @@
-import {sequalize} from '../dbConnection.js'
+import {sequelize} from '../dbConnection.js'
 import pkg from "sequelize";
 const {DataTypes} = pkg
 
-export const User = sequalize.define('user', {
+export const UserModel = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     email: {type: DataTypes.STRING, unique: true},
     password: {type: DataTypes.STRING},
+    firstName: {type: DataTypes.STRING},
+    lastName: {type: DataTypes.STRING},
     role: {type: DataTypes.STRING, defaultValue: 'USER'},
-    avatar: {type: DataTypes.STRING},
-    articles: {type: DataTypes.RANGE(DataTypes.INTEGER)},
-    comments: {type: DataTypes.RANGE(DataTypes.INTEGER)},
+    status: {type: DataTypes.STRING},
+    avatar: {type: DataTypes.STRING, defaultValue: false},
+    userArticles: {type: DataTypes.STRING},
+    comments: {type: DataTypes.STRING},
 })
 
-export const Article = sequalize.define('user', {
+export const ArticleModel = sequelize.define('article', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     title: {type: DataTypes.STRING},
     titleParagraph: {type: DataTypes.STRING},
-    role: {type: DataTypes.STRING, defaultValue: 'USER'},
-    image: {type: DataTypes.STRING},
+    titleImage: {type: DataTypes.STRING},
     content: {type: DataTypes.STRING},
     likeCount: {type: DataTypes.STRING},
     comments: {type: DataTypes.RANGE(DataTypes.INTEGER)},
 })
 
-User.hasMany(Article)
-Article.belongsTo(User)
+UserModel.hasMany(ArticleModel)
+ArticleModel.belongsTo(UserModel)
