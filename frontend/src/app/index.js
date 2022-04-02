87b14d380e5597c93470651a97340a3c2109aa13
George <g-sharikov@bk.ru>
2022-04-02 21:13:58 +0300

    Check auth request

--- frontend/src/app/index.js
+++ frontend/src/app/index.js
@@ -1,8 +1,17 @@
 import {Routing} from '../pages';
 import {Header} from '../widgets/Header';
 import './index.css';
+import {useEffect} from "react";
+import { useDispatch } from 'react-redux';
+import {checkUserAuth} from '../store/userAuthSlice/slice'
 
 export function App() {
+  const dispatch = useDispatch()
+  
+  useEffect(() => {
+      dispatch(checkUserAuth())
+  }, [dispatch])
+
   return (
     <div className={'app'}>
       <Header />
