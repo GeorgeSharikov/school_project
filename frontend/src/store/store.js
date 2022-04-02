923df78cd5df33f3460565c65b8f5efd4884ddcb
George <g-sharikov@bk.ru>
2022-04-02 21:09:15 +0300

    Added userAuth slice to store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import {sideBarToggleSlice} from "../features/sidebarToggle/model/slice.js";
+import { userAuthSlice } from "./userAuthSlice/slice.js";
 
 export const store = configureStore({
     reducer: {
-        sideBarToggle: sideBarToggleSlice
+        sideBarToggle: sideBarToggleSlice,
+        userAuth: userAuthSlice
     }
 })
