854b25dc0affde19ee484ebb7c70dccfcaa8f4c7
George <g-sharikov@bk.ru>
2022-04-02 21:05:42 +0300

    Changed token's time expire

--- server/core/helpers/generateJWT.js
+++ server/core/helpers/generateJWT.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
 
 export const generateJWT = (id, email, role) => {
-    return jwt.sign({id, email, role}, process.env.SECRET_KEY, {expiresIn: '24h'})
+    return jwt.sign({id, email, role}, process.env.SECRET_KEY, {expiresIn: '14d'})
 }
