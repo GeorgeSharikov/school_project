51865c5c1cb3a2fa3fcd3588aeb4fdfe4923049f
George <g-sharikov@bk.ru>
2022-04-13 21:08:42 +0300

    Connected personal data slice

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import {sideBarToggleSlice} from "../features/sidebarToggle/model/slice.js";
 import { userAuthSlice } from "./userAuthSlice/slice.js";
+import { personalDataSlice } from "./userPersonalData/slice.js";
 
 export const store = configureStore({
     reducer: {
         sideBarToggle: sideBarToggleSlice,
-        userAuth: userAuthSlice
+        userAuth: userAuthSlice,
+        personalData: personalDataSlice
     }
 })
