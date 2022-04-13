d823b735df20d5b693a67b4f324156ce99ef0143
George <g-sharikov@bk.ru>
2022-04-13 21:11:43 +0300

    Created displaying of avatar in the header

--- frontend/src/features/navigationUser/index.js
+++ frontend/src/features/navigationUser/index.js
@@ -5,23 +5,27 @@ import { userAuthSelectors } from '../../store/userAuthSlice/slice';
 import { NavLink } from 'react-router-dom';
 import styles from './ui/ui.module.css'
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
-import { Modal } from '@mui/material';
+import { Avatar, Modal } from '@mui/material';
 import { Box } from '@mui/system';
 import { modalStyles } from './ui/ui';
 import { LoginForm } from './loginForm/loginForm';
+import { personalDataSelectors } from '../../store/userPersonalData/slice';
 
 export const NavigationUser = (props) => {
     const [isModalVisible, setModalVisible] = useState(false)
-    // const isAuth = useSelector(state => userAuthSelectors.getIsUserAuth(state))
-    const isAuth = false
+
+    const id = useSelector(state => userAuthSelectors.getUserPersonalId(state))
+    const isAuth = useSelector(state => userAuthSelectors.getIsUserAuth(state))
+    const userData = useSelector(state => personalDataSelectors.getPeronalData(state))
 
     const openHandler = () => setModalVisible(true)
     const closeHandler = () => setModalVisible(false)
     return (
         <div>
             {isAuth 
-            ? <NavLink to='/profile:id'>
-                <AccountBoxIcon sx={{fontSize: '40px', color: 'black', cursor: 'pointer'}}/>
+            ? <NavLink to={`/profile:${id}`}>
+                {/* <AccountBoxIcon sx={{fontSize: '40px', color: 'black', cursor: 'pointer'}}/> */}
+                <Avatar>{userData.firstName}</Avatar>
             </NavLink>
             : <div className={styles.signIn} onClick={openHandler}>
                 <PersonOutlineIcon sx={{marginRight: '8px', fontSize: '30px'}}/>
