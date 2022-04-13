e23e16e0caf360820f14181e1d2497b42cfca622
George <g-sharikov@bk.ru>
2022-04-13 21:10:23 +0300

    Profile page carcass

--- frontend/src/pages/Profile/index.jsx
+++ frontend/src/pages/Profile/index.jsx
@@ -1,6 +1,26 @@
+import {useParams} from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import { userAuthSelectors } from "../../store/userAuthSlice/slice";
+import { getOtherPersonalData, personalDataSelectors } from "../../store/userPersonalData/slice";
+import { useEffect } from "react";
 
 export const Profile = (props) => {
+    const dispatch = useDispatch()
+
+    const {id} = useParams()
+    const myId = useSelector(state => userAuthSelectors.getUserPersonalId(state)) 
+    const isMyOwn = id === myId
+    useEffect(() => {
+        if(!isMyOwn){
+            dispatch(getOtherPersonalData({id}))
+        }
+    }, [isMyOwn,id,dispatch])
+
+    const userData = useSelector(state => personalDataSelectors.getPeronalData(state, isMyOwn))
+    console.log(userData)
     return (
-        <div></div>
+        <div>
+            asdasd
+        </div>
     );
 }
