0dde8b372abd15ea00984e9d4d3abc30dec65a1c
George <g-sharikov@bk.ru>
2022-04-13 21:08:20 +0300

    Added getting user id selector

--- frontend/src/store/userAuthSlice/selectors.js
+++ frontend/src/store/userAuthSlice/selectors.js
@@ -1,2 +1,3 @@
 export const getIsUserAuth = (state) => state.userAuth.isAuth
 export const getIsLoginError = (state) => state.userAuth.errorLoginMessage
+export const getUserPersonalId = (state) => state.userAuth.userId
