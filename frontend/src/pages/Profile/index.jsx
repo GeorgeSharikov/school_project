efe4a705c1aa5640e69dc936c023ccaaeeeb3014
George <g-sharikov@bk.ru>
2022-04-24 20:57:10 +0300

    Created basic layout of profile

--- frontend/src/pages/Profile/index.jsx
+++ frontend/src/pages/Profile/index.jsx
@@ -1,9 +1,11 @@
-import {useParams} from "react-router-dom";
+import {Link, Outlet, useParams} from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 import { userAuthSelectors } from "../../store/userAuthSlice/slice";
 import { getOtherPersonalData, getPersonalData, personalDataSelectors } from "../../store/userPersonalData/slice";
 import { useEffect } from "react";
-
+import styles from './ui.module.css'
+import { ProfileAvatar } from "../../features/avatar";
+import { ProfileStatus } from "../../features/status";
 
 export const Profile = (props) => {
     const dispatch = useDispatch()
@@ -20,12 +22,28 @@ export const Profile = (props) => {
     }, [isMyOwn,id,dispatch])
 
     const userData = useSelector(state => personalDataSelectors.getPeronalDataSelector(state, isMyOwn))
+    console.log(userData)
     return (
-        <div>
-            {id}
-            {userData?.email}
-            {userData?.firstName}
-            {userData?.lastName}
+        <div className={styles.wrapper}>
+            <div className={styles.profileHeaderWrapper}>
+                    <ProfileAvatar ava={userData.avatar}/>
+                    <h1 className={styles.name}>{userData.firstName}{userData.lastName}</h1>
+                    <ProfileStatus status={userData.status}/>
+                    <div className={styles.headerTabs}>
+                        <div className={styles.tabsList}>
+                            <Link to='' className={styles.tab}>
+                                <span>Статьи</span>
+                            </Link>
+                            <Link to='drafts' className={styles.tab}>
+                                <span>Черновики</span>
+                            </Link> 
+                        </div>
+                    </div>
+            </div>
+
+            <div className={styles.profileContentWrapper}>
+                <Outlet />
+            </div>
         </div>
     );
 }
