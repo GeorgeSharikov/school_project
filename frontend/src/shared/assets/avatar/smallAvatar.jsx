8740449bd0a4e16d28cc941bb68f5a3d6bc6a21f
George <g-sharikov@bk.ru>
2022-05-07 01:56:38 +0300

    Small avatar reusable component

---
+++ frontend/src/shared/assets/avatar/smallAvatar.jsx
@@ -0,0 +1,15 @@
+import { Avatar } from "@mui/material"
+import { useSelector } from "react-redux"
+import { getPeronalDataSelector } from "../../../store/userPersonalData/selectors"
+import styles from './ui.module.css'
+
+export const SmallAvatar = () => {
+    const {avatar, firstName} = useSelector(state => getPeronalDataSelector(state))
+    const firstLetter = firstName.split('')[0]
+    console.log(!!!avatar)
+    return (
+        !!!avatar 
+        ? <div style={{backgroundImage: `url(${avatar})`}} className={styles.subImg}/>
+        : <Avatar style={{width: '22px', height: '22px'}}className={styles.ava}>{firstLetter}</Avatar>
+    )
+}
