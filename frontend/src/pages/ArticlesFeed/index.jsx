871a84ce1c2335232d1ad1a82c2f4bb4e63036cf
George <g-sharikov@bk.ru>
2022-05-07 01:59:22 +0300

    Change moke data

--- frontend/src/pages/ArticlesFeed/index.jsx
+++ frontend/src/pages/ArticlesFeed/index.jsx
@@ -7,8 +7,7 @@ export const ArticlesFeed = () => {
         title: 'Фильм HBO и CNN о Навальном получил две награды на фестивале',
         prewords: 'В номинации Festival Favorite Award и приз зрительских симпатий. В номинации Festival Favorite Award и приз зрительских симпатийaaaaaaaaaaaaaaaa. В номинации Festival Favorite Award и приз зрительских симпатий.',
         image: 'https://leonardo.osnova.io/b5430f91-7c06-5bfb-a7f7-86ff5edc9b4c/-/preview/1100/-/format/webp/',
-        author: 'Pupkin Zalupkin',
-        sub: 'Dvach',
+        author: 'Pupkin lupkin',
         subImage: 'https://leonardo.osnova.io/2810b9bb-071f-8a49-2290-2f92ca6797cd/-/scale_crop/64x64/',
         date: 'Today',
         likesCount: 50
