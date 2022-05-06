be0b7eef2c27f6b55da92006ee47947b810448eb
George <g-sharikov@bk.ru>
2022-05-07 02:03:50 +0300

    Changed ui and added some features

--- frontend/src/features/createArticle/index.js
+++ frontend/src/features/createArticle/index.js
@@ -2,12 +2,14 @@ import styles from './ui/styles.module.css'
 import EditorJS from "@editorjs/editorjs";
 import {settings} from "../../constants/editorJsSetteng.js";
 import {useEffect, useRef} from "react";
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Textarea from 'react-expanding-textarea'
+import { SmallAvatar } from '../../shared/assets/avatar/smallAvatar';
 
 export const ArticleEditor = (props) => {
     const editor = new EditorJS(settings)
     const inputRef = useRef()
-
+    const editorRef = useRef()
+    
     const save = async () => {
         try{
             const data = await editor.save()
@@ -18,45 +20,56 @@ export const ArticleEditor = (props) => {
         }
     }
 
+    const onTitleChange = (event) => {
+        if(event.keyCode === 13){
+            event.preventDefault()
+            editor.focus()
+        }
+    }
+
     useEffect(() => {
         inputRef.current.focus()
-        document.getElementById("editorjs").childNodes[0].remove()
+        setTimeout(() => {
+            editorRef.current.removeChild(editorRef.current.children[0])
+        }, 0)
+        return () => {
+            editor.destroy()
+        }
+        // eslint-disable-next-line
     }, [])
-
     return (
         <div className={styles.editorContainer}>
-            <div className={`${styles.Ieditor} ${styles.editorAuthors}`}>
-                <div className={styles.authorSelect}>
-                    <span className={styles.selectImg}>
-                        <img src={'https://leonardo.osnova.io/aa291fee-0c9a-4803-c179-af2bda5cb297/'} alt={'sub'}/>
-                    </span>
-                    <span className={styles.authorName}>
-                        Мой блог
-                    </span>
-                    <span className={styles.expandIcon}>
-                        <ExpandMoreIcon style={{width: 20, height: 20}}/>
-                    </span>
-                </div>
-            </div>
-            <div className={styles.editorContent}>
-                <div className={styles.Ieditor}>
-                    <textarea rows="1"
-                              id={'titleArea'}
-                              ref={inputRef}
-                              placeholder="Заголовок"
-                              maxLength="120"
-                              className={styles.inputTitle}
-                              style={{height: 47, overflowY: 'hidden'}}
-                              key={1}/>
-                </div>
-                <div id={'editorjs'} className={`${styles.editorJS}`}/>
-            </div>
             <div className={`${styles.Ieditor} ${styles.editorBottom}`}>
                 <div className={styles.bottomContent}>
                     <button onClick={save} className={styles.publishButton}>
                         <span className={styles.buttonLabel}>Опубликовать</span>
                     </button>
                 </div>
+            </div>
+            <div className={`${styles.Ieditor} ${styles.editorAuthors}`}>
+                <div className={styles.authorSelect}>
+                    <span className={styles.selectImg}>
+                        <SmallAvatar />
+                    </span>
+                    <span className={styles.authorName}>
+                        Мой блог
+                    </span>
+                </div>
+            </div>
+            <div className={styles.editorContent}>
+                <div className={styles.Ieditor}>
+                    <Textarea rows="1"
+                                onKeyDown={onTitleChange}
+                                id={'titleArea'}
+                                ref={inputRef}
+                                placeholder="Заголовок"
+                                maxLength="120"
+                                className={styles.inputTitle}
+                                style={{minHeight: 47, overflowY: 'hidden'}}
+                                
+                              />
+                </div>
+                <div ref={editorRef} id={'editorjs'} className={`${styles.editorJS}`}/>
             </div>
         </div>
 
