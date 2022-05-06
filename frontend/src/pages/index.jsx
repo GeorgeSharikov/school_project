ca23da0518d09ecf35455facf17a47a35d08c852
George <g-sharikov@bk.ru>
2022-05-07 02:05:40 +0300

    Deleted writing route

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -4,7 +4,6 @@ import styles from './main.module.css'
 import {useSelector} from "react-redux";
 import {sideBarSelectors} from "../features/sidebarToggle/model/slice.js";
 import {ArticlesFeed} from "./ArticlesFeed";
-import {ArticleEditorPage} from "./ArticleEditor";
 import { Profile } from "./Profile";
 import { ProfileArticleFeed } from "../features/profileArticleFeed";
 import { ProfileDraftsFeed } from "../features/profileDraftsFeed";
@@ -17,14 +16,13 @@ export const Routing = () => {
             <div className={styles.content}>
                     <Routes>
                         <Route path={'/'} element={<ArticlesFeed />}/>         
-                        <Route path={'/writing'} element={<ArticleEditorPage />}/>
                         <Route path={'/profile/:id'} element={<Profile/>}>
                             <Route path="" element={<ProfileArticleFeed />} />
                             <Route path="drafts" element={<ProfileDraftsFeed />} />
                         </Route>
                     </Routes>
             </div>
-            <div style={{width: '145px'}}>a</div>
+            <div style={{width: '145px'}}></div>
         </div>
     )
 }   
