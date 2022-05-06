0e4ae85457b01c8dcad1fdaa5778315973b45167
George <g-sharikov@bk.ru>
2022-05-07 02:06:35 +0300

    Deleted author avatar from post card

--- frontend/src/entities/Post/ui/PostHeader/index.jsx
+++ frontend/src/entities/Post/ui/PostHeader/index.jsx
@@ -4,10 +4,10 @@ import {NavLink} from "react-router-dom";
 export const PostHeader = ({author, sub, subImage, date}) => {
     return (
         <div className={styles.header}>
-            <NavLink to={'/sub'} className={`${styles.headerItem} ${styles.author}`}>
+            {/* <NavLink to={'/sub'} className={`${styles.headerItem} ${styles.author}`}>
                 <div style={{backgroundImage: `url(${subImage})`}} className={styles.subImg}/>
                 <div>{sub}</div>
-            </NavLink>
+            </NavLink> */}
             <NavLink to={'/author'} className={styles.headerItem}>
                 <div>{author}</div>
             </NavLink>
