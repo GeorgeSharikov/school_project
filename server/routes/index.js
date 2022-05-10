8301ed543aba198c6490ee3a93377e7f89dc81af
George <g-sharikov@bk.ru>
2022-05-10 17:07:58 +0300

    Added upload image endpoint endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 import {Router} from 'express'
 import { userInfoRouter } from './userInfoRoutes.js';
 import {userRouter} from "./userRoutes.js";
+import {uploadFileRouter} from "./uploadFileRouter.js";
 
 const router = new Router()
 
 router.use('/user', userRouter)
 router.use('/userInfo', userInfoRouter)
+router.use('/static', uploadFileRouter)
 
 export const mainRouter = router
