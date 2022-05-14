f25d5f6a82a3c9da2b7be9a49b59c09d087e3b56
George <g-sharikov@bk.ru>
2022-05-14 16:05:36 +0300

    Showing login form after pressing of add article button if user isn't logged

--- frontend/src/features/addArticle/index.jsx
+++ frontend/src/features/addArticle/index.jsx
@@ -2,14 +2,15 @@ import {Button, LinkStyled} from "./ui/StyledComponents.js";
 import Typography from "@mui/material/Typography";
 import { userAuthSelectors } from "../../store/userAuthSlice/slice.js";
 import { useSelector } from "react-redux";
-import { useState } from "react";
+import React, { useState } from "react";
 import { ArticleEditorModal } from "../createArticle/ModalArticleEditor/index.jsx";
+import {LoginModal} from "../loginModal/index.js";
 
 export const AddArticleButton = () => {
     const isAuth = useSelector(state => userAuthSelectors.getIsUserAuth(state))
     const [isOpen, setIsOpen] = useState(false)
     
-    const hadnleClose = () => setIsOpen(!isOpen)
+    const handleClose = () => setIsOpen(!isOpen)
     return (
         <>
             <Button>
@@ -22,7 +23,13 @@ export const AddArticleButton = () => {
                     </Typography>
                 </LinkStyled>
             </Button>
-            <ArticleEditorModal isVisible={isOpen} hadnleClose={hadnleClose}/>
+            {isAuth
+                ? <ArticleEditorModal isVisible={isOpen} handleClose={handleClose}/>
+                : isOpen ? <LoginModal isModalVisible={isOpen}
+                                       closeHandler={() => setIsOpen(false)}
+                                       setModalVisible={setIsOpen}/> : null
+            }
+
         </>
 
     )
