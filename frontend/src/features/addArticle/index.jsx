a80e7cd243809e81640a6d065b405872a5963cf5
George <g-sharikov@bk.ru>
2022-05-20 16:35:36 +0300

    Use reducer login variables instead of locals and separate open variables between editor and login modals

--- frontend/src/features/addArticle/index.jsx
+++ frontend/src/features/addArticle/index.jsx
@@ -1,20 +1,38 @@
 import {Button, LinkStyled} from "./ui/StyledComponents.js";
 import Typography from "@mui/material/Typography";
-import { userAuthSelectors } from "../../store/userAuthSlice/slice.js";
+import {userAuthActions, userAuthSelectors} from "../../store/userAuthSlice/slice.js";
 import { useSelector } from "react-redux";
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import { ArticleEditorModal } from "../createArticle/ModalArticleEditor/index.jsx";
 import {LoginModal} from "../loginModal/index.js";
+import {useActions} from "../../shared/hooks/useActions.jsx";
 
 export const AddArticleButton = () => {
+    const {setLoginIsOpen} = useActions(userAuthActions)
+    const isLoginModalOpen = useSelector(state => userAuthSelectors.getIsLoginModalOpen(state))
     const isAuth = useSelector(state => userAuthSelectors.getIsUserAuth(state))
-    const [isOpen, setIsOpen] = useState(false)
-    
-    const handleClose = () => setIsOpen(!isOpen)
+
+    const [isLoginOpen, setIsLoginOpen] = useState(isLoginModalOpen)
+    const [isOpenEditorOpen, setIsOpenEditorOpen] = useState(false)
+
+    const handleEditorModalClose = () => setIsOpenEditorOpen(!isOpenEditorOpen)
+
+    const handleOpen = () => {
+        if(isAuth){
+            setIsOpenEditorOpen(true)
+        }else{
+            setLoginIsOpen(true)
+        }
+    }
+
+    useEffect(() => {
+        setIsLoginOpen(isLoginModalOpen)
+    }, [isLoginModalOpen])
+
     return (
         <>
             <Button>
-                <LinkStyled onClick={() => setIsOpen(true)}>
+                <LinkStyled onClick={handleOpen}>
                     <Typography
                         sx={{p: '0 16px'}}
                         component={'div'}
@@ -24,12 +42,11 @@ export const AddArticleButton = () => {
                 </LinkStyled>
             </Button>
             {isAuth
-                ? <ArticleEditorModal isVisible={isOpen} handleClose={handleClose}/>
-                : isOpen ? <LoginModal isModalVisible={isOpen}
-                                       closeHandler={() => setIsOpen(false)}
-                                       setModalVisible={setIsOpen}/> : null
+                ? isOpenEditorOpen ? <ArticleEditorModal isVisible={isOpenEditorOpen} handleClose={handleEditorModalClose}/> : null
+                : isLoginOpen ? <LoginModal isModalVisible={isLoginOpen}
+                                       closeHandler={() => setLoginIsOpen(false)}
+                                       /> : null
             }
-
         </>
 
     )
