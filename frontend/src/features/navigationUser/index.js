f34388cbe68663ea5737a0afbda201a800591f64
George <g-sharikov@bk.ru>
2022-05-21 17:16:42 +0300

    Added skeleton component while fetching

--- frontend/src/features/navigationUser/index.js
+++ frontend/src/features/navigationUser/index.js
@@ -7,6 +7,7 @@ import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import { Avatar} from '@mui/material';
 import { personalDataSelectors } from '../../store/userPersonalData/slice';
 import { LoginModal } from '../loginModal';
+import Skeleton from "@mui/material/Skeleton";
 
 export const NavigationUser = (props) => {
     const [isModalVisible, setModalVisible] = useState(false)
@@ -14,24 +15,27 @@ export const NavigationUser = (props) => {
     const id = useSelector(state => userAuthSelectors.getUserPersonalId(state))
     const isAuth = useSelector(state => userAuthSelectors.getIsUserAuth(state))
     const userData = useSelector(state => personalDataSelectors.getPeronalDataSelector(state))
+    const isFetching = useSelector(state => personalDataSelectors.getIsFetching(state))
 
     const openHandler = () => setModalVisible(true)
     const closeHandler = () => setModalVisible(false)
     return (
         <div>
-            {isAuth 
-            ? <NavLink to={`/profile/${id}`}>
-                <Avatar>{userData.firstName}</Avatar>
-            </NavLink>
-            : <div className={styles.signIn} onClick={openHandler}>
-                <PersonOutlineIcon sx={{marginRight: '8px', fontSize: '30px'}}/>
-                <span>
-                    Войти
-                </span>
-            </div>}
-        {isModalVisible 
-        ? <LoginModal isModalVisible={isModalVisible} closeHandler={closeHandler} setModalVisible={setModalVisible}/>
-        : null}
+            {isFetching
+                ? <Skeleton variant="rectangular" width={140} height={30} />
+                : isAuth ? <NavLink to={`/profile/${id}`}>
+                                <Avatar>{userData.firstName}</Avatar>
+                    </NavLink>
+                    : <div className={styles.signIn} onClick={openHandler}>
+                        <PersonOutlineIcon sx={{marginRight: '8px', fontSize: '30px'}}/>
+                        <span>
+                            Войти
+                        </span>
+                    </div>
+            }
+            {isModalVisible
+            ? <LoginModal isModalVisible={isModalVisible} closeHandler={closeHandler} setModalVisible={setModalVisible}/>
+            : null}
         </div>
     );
 }
