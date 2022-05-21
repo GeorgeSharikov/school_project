79b89976bbaff45601772bc1e5bf3162151fdf62
George <g-sharikov@bk.ru>
2022-05-21 20:25:26 +0300

    Insert html via dangerouslySetInnerHTML

--- frontend/src/entities/Post/ui/PostContent/index.jsx
+++ frontend/src/entities/Post/ui/PostContent/index.jsx
@@ -1,19 +1,26 @@
 import styles from './styles.module.css'
 
-export const PostContent = ({title, prewords, image}) => {
+export const PostContent = ({title, titleParagraph, secondTitleBlock}) => {
+
+    const setHtml = (html) => {
+        return {__html: html}
+    }
     return (
         <div>
-            <div className={styles.title}>
-                {title}
-            </div>
-            <div className={styles.prewords}>
-                <p>{prewords}</p>
-            </div>
-            <div className={styles.mediaContainer}>
-                <div className={styles.mediaContent}>
-                    <img src={image} alt={'content art'}/>
-                </div>
-            </div>
+            <div dangerouslySetInnerHTML={setHtml(title)}/>
+            <div dangerouslySetInnerHTML={setHtml(titleParagraph)}/>
+            <div dangerouslySetInnerHTML={setHtml(secondTitleBlock)}/>
+            {/*<div className={styles.title}>*/}
+            {/*    {title}*/}
+            {/*</div>*/}
+            {/*<div className={styles.prewords}>*/}
+            {/*    <p>{prewords}</p>*/}
+            {/*</div>*/}
+            {/*<div className={styles.mediaContainer}>*/}
+            {/*    <div className={styles.mediaContent}>*/}
+            {/*        <img src={image} alt={'content art'}/>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
         </div>
     )
 }
