9c2d9f981d81ccf64b9dc6bd05e004d26d6ba782
George <g-sharikov@bk.ru>
2022-05-21 20:21:28 +0300

    Connect articles slice

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import {sideBarToggleSlice} from "../features/sidebarToggle/model/slice.js";
 import { userAuthSlice } from "./userAuthSlice/slice.js";
 import { personalDataSlice } from "./userPersonalData/slice.js";
+import {articleSlice} from "../features/showArticles/model/slice.js";
 
 export const store = configureStore({
     reducer: {
         sideBarToggle: sideBarToggleSlice,
         userAuth: userAuthSlice,
-        personalData: personalDataSlice
+        personalData: personalDataSlice,
+        article: articleSlice
     }
 })
