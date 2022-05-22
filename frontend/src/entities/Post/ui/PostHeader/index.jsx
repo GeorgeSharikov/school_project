b0d451afa94695003a00c9d29c8506c6431a9b80
George <g-sharikov@bk.ru>
2022-05-22 21:55:20 +0300

    Added user avatar to post

--- frontend/src/entities/Post/ui/PostHeader/index.jsx
+++ frontend/src/entities/Post/ui/PostHeader/index.jsx
@@ -1,14 +1,16 @@
 import styles from './styles.module.css'
 import {NavLink} from "react-router-dom";
+import Avatar from "@mui/material/Avatar";
+import {toHumanDateFormat} from "../../../../shared/helpers/toHumanDateFormat.js";
 
 export const PostHeader = ({author,date, authorId}) => {
+    // toHumanDateFormat(date)
     return (
         <div className={styles.header}>
-            {/* <NavLink to={'/sub'} className={`${styles.headerItem} ${styles.author}`}>
-                <div style={{backgroundImage: `url(${subImage})`}} className={styles.subImg}/>
-                <div>{sub}</div>
-            </NavLink> */}
-            <NavLink to={`/profile/${authorId}`} className={styles.headerItem}>
+            <NavLink to={`/profile/${authorId}`} className={`${styles.headerItem} ${styles.author}`}>
+                <Avatar variant="rounded" className={styles.avatar} style={{width: 22, height: 22}}>
+                    {author[0].toUpperCase()}
+                </Avatar>
                 <div>{author}</div>
             </NavLink>
             <div className={styles.time}>
