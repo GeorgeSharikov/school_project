600cd05c302dd2efd8636abd2d04b33e95a6ac7b
George <g-sharikov@bk.ru>
2022-05-26 02:30:44 +0300

    Added styles for images in the feed

--- frontend/src/entities/Post/ui/PostContent/index.jsx
+++ frontend/src/entities/Post/ui/PostContent/index.jsx
@@ -12,17 +12,6 @@ export const PostContent = ({title, titleParagraph, secondTitleBlock}) => {
             </div>
             <div dangerouslySetInnerHTML={setHtml(titleParagraph)}/>
             <div dangerouslySetInnerHTML={setHtml(secondTitleBlock)}/>
-            {/*<div className={styles.title}>*/}
-            {/*    {title}*/}
-            {/*</div>*/}
-            {/*<div className={styles.prewords}>*/}
-            {/*    <p>{prewords}</p>*/}
-            {/*</div>*/}
-            {/*<div className={styles.mediaContainer}>*/}
-            {/*    <div className={styles.mediaContent}>*/}
-            {/*        <img src={image} alt={'content art'}/>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
         </div>
     )
 }
