39f218aea0f9f8349b3383f1e8faafabd92c2597
George <g-sharikov@bk.ru>
2022-05-28 22:17:03 +0300

    Connect get article slice

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -3,12 +3,14 @@ import {sideBarToggleSlice} from "../features/sidebarToggle/model/slice.js";
 import { userAuthSlice } from "./userAuthSlice/slice.js";
 import { personalDataSlice } from "./userPersonalData/slice.js";
 import {articleSlice} from "../features/showArticles/model/slice.js";
+import {articleInfoSlice} from "../features/getArticle/slice.js";
 
 export const store = configureStore({
     reducer: {
         sideBarToggle: sideBarToggleSlice,
         userAuth: userAuthSlice,
         personalData: personalDataSlice,
-        article: articleSlice
+        article: articleSlice,
+        getArticle: articleInfoSlice
     }
 })
