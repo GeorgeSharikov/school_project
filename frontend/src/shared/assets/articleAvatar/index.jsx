34231cef630313f12eb3d8e18394a854b1846bdf
George <g-sharikov@bk.ru>
2022-05-30 03:44:17 +0300

    Added width and height settings

--- frontend/src/shared/assets/articleAvatar/index.jsx
+++ frontend/src/shared/assets/articleAvatar/index.jsx
@@ -3,10 +3,10 @@ import {NavLink} from "react-router-dom";
 import styles from './styles.module.css'
 import Avatar from "@mui/material/Avatar";
 
-export const ArticleAvatar = ({author, authorId}) => {
+export const ArticleAvatar = ({author, authorId, sizeW = 22, sizeH = 22}) => {
     return (
         <NavLink to={`/profile/${authorId}`} className={`${styles.headerItem} ${styles.author}`}>
-            <Avatar variant="rounded" className={styles.avatar} style={{width: 22, height: 22}}>
+            <Avatar variant="rounded" className={styles.avatar} style={{width: sizeW, height: sizeH}}>
                 {author[0].toUpperCase()}
             </Avatar>
             <div>{author}</div>
