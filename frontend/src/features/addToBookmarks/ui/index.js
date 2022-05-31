2113d50ad6097e9157ed9c3c6d1bdbf52d5f72d6
George <g-sharikov@bk.ru>
2022-05-31 13:43:53 +0300

    Added login form on click if not auth

--- frontend/src/features/addToBookmarks/ui/index.js
+++ frontend/src/features/addToBookmarks/ui/index.js
@@ -4,14 +4,17 @@ import BookmarkIcon from '@mui/icons-material/Bookmark';
 import styles from './styles.module.css'
 import {useDispatch, useSelector} from "react-redux";
 import {addBookmark, articleSelectors} from "../../showArticles/model/slice.js";
+import {userAuthSelectors} from "../../../store/userAuthSlice/slice.js";
+import {LoginModal} from "../../loginModal/index.js";
 
 
 export const AddToBookMarks = ({articleId, authUserId}) => {
+    const [isOpen, setIsOpen] = useState(false)
     articleId = String(articleId)
     const dispatch = useDispatch()
     const userBookmarks = useSelector(state => articleSelectors.getUserBookmarks(state))
+    const isAuth = useSelector(state => userAuthSelectors.getIsUserAuth(state))
     const [isInBookmarks, setIsInBookmarks] = useState(false)
-
     useEffect(() => {
         if(userBookmarks.includes(articleId)){
             setIsInBookmarks(true)
@@ -19,6 +22,9 @@ export const AddToBookMarks = ({articleId, authUserId}) => {
     }, [userBookmarks])
 
     const addToBookMarks = () => {
+        if(!isAuth){
+            return setIsOpen(true)
+        }
         dispatch(addBookmark(articleId))
         if(isInBookmarks){
             setIsInBookmarks(false)
@@ -26,13 +32,14 @@ export const AddToBookMarks = ({articleId, authUserId}) => {
             setIsInBookmarks(true)
         }
     }
-    console.log(articleId, isInBookmarks)
+    console.log(userBookmarks, userBookmarks.includes(articleId))
     return (
         <div className={styles.bookmark}>
             {isInBookmarks
                 ? <BookmarkIcon className={`${styles.bookmarkIcon} ${styles.bookmarkAddedColor}`} onClick={addToBookMarks}/>
                 : <BookmarkBorderIcon className={styles.bookmarkIcon} onClick={addToBookMarks}/>
             }
+            {isOpen && <LoginModal isModalVisible={isOpen} setModalVisible={setIsOpen} closeHandler={() => setIsOpen(false)}/>}
         </div>
     );
 }
