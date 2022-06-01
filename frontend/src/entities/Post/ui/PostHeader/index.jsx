d3ff2d3dd59fa86d3803d164d910bdbb592ccfa7
George <g-sharikov@bk.ru>
2022-06-01 04:34:18 +0300

    Added options

--- frontend/src/entities/Post/ui/PostHeader/index.jsx
+++ frontend/src/entities/Post/ui/PostHeader/index.jsx
@@ -3,13 +3,17 @@ import {NavLink} from "react-router-dom";
 import Avatar from "@mui/material/Avatar";
 import {ArticleAvatar} from "../../../../shared/assets/articleAvatar/index.jsx";
 import {ArticleDate} from "../../../../shared/assets/articleDate/index.jsx";
+import {PostActions} from "../../../../features/postActions/index.jsx";
 
-export const PostHeader = ({author,date, authorId}) => {
+export const PostHeader = ({author,date, authorId, showActions, showEditAction, showDelAction}) => {
     // toHumanDateFormat(date)
     return (
         <div className={styles.header}>
-            <ArticleAvatar author={author} authorId={authorId}/>
-            <ArticleDate date={date}/>
+            <div className={styles.info}>
+                <ArticleAvatar author={author} authorId={authorId}/>
+                <ArticleDate date={date}/>
+            </div>
+            {showActions && <PostActions showDelAction={showDelAction} showEditAction={showEditAction}/>}
         </div>
     );
 }
