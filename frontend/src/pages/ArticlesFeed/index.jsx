eb78ad01546da1e3f9eb0540a785122e89248783
George <g-sharikov@bk.ru>
2022-06-01 21:07:13 +0300

    Replaced <ShowArticles /> by <BlockOfArticles />

--- frontend/src/pages/ArticlesFeed/index.jsx
+++ frontend/src/pages/ArticlesFeed/index.jsx
@@ -1,10 +1,27 @@
 import styles from './styles.module.css'
-import {ShowArticles} from "../../features/showArticles/index.js";
+import {BlockOfArticle} from "../../entities/BlockOfArticle/index.jsx";
+import {
+    articleActions,
+    articleSelectors,
+    getArticlesTotalCount,
+    getFeedArticles
+} from "../../features/showArticles/model/slice.js";
+import React from "react";
+import {useActions} from "../../shared/hooks/useActions.jsx";
 
 export const ArticlesFeed = () => {
+    const {setFeedArticles} = useActions(articleActions)
     return (
         <div className={styles.wrapper}>
-            <ShowArticles />
+                <BlockOfArticle getArticles={(page) => getFeedArticles(page)}
+                                amount={5}
+                                getTotalCount={() => getArticlesTotalCount({isModerated: true, id: null})}
+                                showDelOpt={false}
+                                showOpt={false}
+                                showEditOpt={false}
+                                setter={(arr) => setFeedArticles(arr)}
+                                selectors={articleSelectors}
+                />
         </div>
     )
 }
