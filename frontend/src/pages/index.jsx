22cc8b172cd02029b1c2292f9647c87ad7590a43
George <g-sharikov@bk.ru>
2022-06-01 04:37:04 +0300

    Added styles fro right part

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -2,19 +2,27 @@ import {Routes, Route} from "react-router-dom";
 import {SideBar} from "../widgets/Sidebar";
 import styles from './main.module.css'
 import {useSelector} from "react-redux";
-import {sideBarSelectors} from "../features/sidebarToggle/model/slice.js";
+import {sideBarSelectors, toggleActions} from "../features/sidebarToggle/model/slice.js";
 import {ArticlesFeed} from "./ArticlesFeed";
 import { Profile } from "./Profile";
-import { ProfileArticleFeed } from "../features/profileArticleFeed";
-import { ProfileDraftsFeed } from "../features/profileDraftsFeed";
+import { ProfileArticleFeed } from "./profileArticleFeed";
+import { ProfileDraftsFeed } from "./profileDraftsFeed";
 import {FullArticle} from "./FullArticle/index.js";
 import {PageNotFound} from "./PageNotFound/index.js";
+import {useActions} from "../shared/hooks/useActions.jsx";
+import {useEffect} from "react";
 
 export const Routing = () => {
     const isSidebarActive = useSelector(sideBarSelectors.getSidebarIsActive)
+    const {setActive} = useActions(toggleActions)
+    useEffect(() => {
+        if(window.innerWidth < 859 && isSidebarActive){
+            setActive()
+        }
+    }, [])
     return (
         <div className={styles.main}>
-            {isSidebarActive && <SideBar/>}
+            {isSidebarActive && <SideBar />}
             <div className={styles.content}>
                     <Routes>
                         <Route path={'/'} element={<ArticlesFeed />}/>
@@ -24,9 +32,10 @@ export const Routing = () => {
                             <Route path="drafts" element={<ProfileDraftsFeed />} />
                         </Route>
                         <Route path={"/error-page-not-found"} element={<PageNotFound/>}/>
+                        <Route path="*" element={<PageNotFound />} />
                     </Routes>
             </div>
-            <div style={{width: '145px'}}></div>
+            <div className={styles.rightPart}></div>
         </div>
     )
 }   
