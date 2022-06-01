2de9b0c5bee8589a1b845655a4df9b83933b61b8
George <g-sharikov@bk.ru>
2022-06-01 04:23:23 +0300

    Added CheckAccess middleware

--- server/middleware/CheckAuthMiddleware.js
+++ server/middleware/CheckAuthMiddleware.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken'
+import env from 'dotenv';env.config()
 
 export const CheckAuth = (req, res, next) => {
     if(req.method === 'OPTIONS'){
@@ -13,5 +14,23 @@ export const CheckAuth = (req, res, next) => {
         next()
     }catch(e){
         res.status(401).json({message:'Не авторизован'})
+    }
+}
+
+export const CheckAccess = (role) => (req, res, next) => {
+    try{
+        const token = req.headers.authorization.split(' ')[1]
+        if(!token){
+            return res.status(401).json({message:'Не авторизован'})
+        }
+        const userInfo = jwt.verify(token, process.env["SECRET_KEY"])
+        if(userInfo['role'] !== 'ADMIN'){
+            return res.status(403).json({message:'Отказано в доступе'})
+        }
+        req.user = jwt.verify(token, process.env["SECRET_KEY"])
+        next()
+    }catch(e){
+        console.log(e)
+        // res.status(401).json({message:'Не авторизован'})
     }
 }
