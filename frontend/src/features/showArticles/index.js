90b9db4b2ca971d2b73e1e115ab4266114998f25
George <g-sharikov@bk.ru>
2022-06-01 04:34:00 +0300

    Used BlockOfArticle

--- frontend/src/features/showArticles/index.js
+++ frontend/src/features/showArticles/index.js
@@ -1,77 +1,16 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import {articleActions, articleSelectors, getArticlesTotalCount, getBookmarks, getFeedArticles} from "./model/slice.js";
-import {PostItem} from "../../entities/Post/ui/PostItem/index.js";
-import Skeleton from "@mui/material/Skeleton";
-import {useActions} from "../../shared/hooks/useActions.jsx";
-import {userAuthSelectors} from "../../store/userAuthSlice/slice.js";
+import React, {} from 'react';
+import {getArticlesTotalCount, getBookmarks, getFeedArticles} from "./model/slice.js";
+import {BlockOfArticle} from "../../entities/BlockOfArticle/index.jsx";
 
 export const ShowArticles = () => {
-    const dispatch = useDispatch()
-    const {setFeedArticles} = useActions(articleActions)
 
-    const authUserId = useSelector(state => userAuthSelectors.getUserPersonalId(state))
-    const articlesFeed = useSelector(state => articleSelectors.getArticles(state))
-    const totalCount = useSelector(state => articleSelectors.totalArticlesCount(state))
-    const [page, setPage] = useState(1)
-    const [loading, setLoading] = useState(false)
-    const [lastElement, setLastElement] = useState(null);
-
-    const observer = useRef(
-        new IntersectionObserver(
-            (entries) => {
-                const first = entries[0];
-                if (first.isIntersecting) {
-                    setPage((no) => no + 1);
-                }
-            })
-    );
-
-    useEffect(() => {
-        const currentElement = lastElement;
-        const currentObserver = observer.current;
-
-        if (currentElement) {
-            currentObserver.observe(currentElement);
-        }
-
-        return () => {
-
-            if (currentElement) {
-                currentObserver.unobserve(currentElement);
-            }
-        };
-    }, [lastElement]);
-
-    useEffect(() => {
-        dispatch(getBookmarks())
-        dispatch(getArticlesTotalCount({isModerated: true, id: null}))
-        return () => {
-            setFeedArticles([])
-        }
-    }, [dispatch])
-    useEffect(() => {
-        const maxPage = Math.ceil(totalCount/5)
-        setLoading(true)
-        if(page <= maxPage || (totalCount < 5 && totalCount !== 0)){
-            dispatch(getFeedArticles(page))
-        }
-        setLoading(false)
-    }, [page, dispatch, totalCount])
     return (
-        <div>
-                {articlesFeed.length > 0 && articlesFeed.map((el, i) => {
-                    if(i === articlesFeed.length-1 && !loading && page*5 <= totalCount ){
-                        return <div key={el.id} ref={setLastElement}>
-                            <PostItem  post={el} authUserId={authUserId}/>
-                        </div>
-                    }
-                    return <PostItem post={el} key={el.id} authUserId={authUserId}/>
-                })}
-            {loading && <div>
-                <Skeleton variant="text" />
-                <Skeleton variant="rectangular" width={640} height={700} />
-            </div>}
-        </div>
+        <BlockOfArticle getArticles={(page) => getFeedArticles(page)}
+                        amount={5}
+                        getTotalCount={() => getArticlesTotalCount({isModerated: true, id: null})}
+                        showDelOpt={false}
+                        showOpt={false}
+                        showEditOpt={false}
+                        />
     );
 }
